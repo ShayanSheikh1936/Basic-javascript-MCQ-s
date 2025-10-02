77093bb0b6a0fd1d613f04d1da1eb0da9c5d72e6
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-10-02 23:35:38 +0500

    basic-javascript-mcqs

---
+++ script.js
@@ -0,0 +1,167 @@
+// // Answer1
+
+// // What is the output of the following code?
+// let name = "John";
+// console.log("Answer no 1)",typeof name);
+// // a) object b) string c) undefined d) boolean
+// //correct b) (String)
+
+// Answer2
+
+// Which of the following is a JavaScript primitive type?
+// a) Array b) Object) Number d) Function
+//correct c) Number
+
+// // Answer 3
+
+// // What will be the output of this code?
+// let age;
+// console.log("Answer no 3)",typeof age);
+// // a) number b) string c) undefined d) boolean
+// // correct C) Undefined
+
+//Answer 4
+
+// How do you access the first element of an array in JavaScript?
+// a) array.first() b) array(0) c) array[0] d) array.get(0
+// correct C array[0]
+
+// // Answer 5
+
+// // What will this code output?
+// let fruits = ["apple", "banana", "mango"];
+// console.log("Answer no 5)",fruits.length);
+// // a) 2 b) 3 c) 4 d) Error
+// // correct b) 3 
+
+// Answer 6
+
+// Which method is used to add an item at the end of an array?
+// a) push() b) pop() c) shift() d) unshift()
+//  correct a) push
+
+// Answer 7
+
+// What will be printed by the following code?
+let x = 10;
+if (x > 5) {
+console.log("Answer no 7)","Greater");
+} else {
+console.log("Answer no 7)","Smaller");
+}
+// a) Greater b) Smaller c) Undefined d) Error
+
+// correct a) Greater
+
+// Anwser 8
+
+// Which symbol is used for 'equal to' in a conditional statement?
+// a) = b) == c) === d) Both b and c
+// correct b) equal
+
+// Answer 9
+
+// What is the output of this code?
+let isLoggedIn = false;
+if (isLoggedIn) {
+console.log("Answer no 9)","Welcome!");
+} else {
+console.log("Answer no 9)","Please log in.");
+}
+// a) Welcome! b) Please log in. c) Error d) Undefined
+// correct b) Please log in
+
+// Answer 10 
+
+// How do you check if a variable `num` is exactly equal to 100 and is a number type?
+// a) if (num = 100 && typeof num === "number")
+// b) if (num == 100 && typeof num = "number")
+// c) if (num === 100 && typeof num === "number")
+// d) if (num === 100 && typeof num = number)
+
+// correct b) if (num == 100 && typeof num = "number")
+
+// Answer 11
+
+// What will be the output of the following code?
+let message = "Hello World";
+console.log(message.indexOf("World"));
+// a) -1 b) 5 c) 6 d) Error
+
+// correct c) 6
+
+// Answer 12
+
+// Which method splits a string into an array using a specific separator?
+// a) split() b) splice() c) join() d) slice()
+
+// correct b) Split()
+
+// Answer 13
+
+// What does the splice() method do in arrays?
+// a) Removes items from an array b) Adds items to an array c) Can add and remove items d) Only
+// duplicates items
+
+// correct c) Can add and remove items
+
+// Answer 14
+
+// What will be the result of this code?
+let colors = ["red", "blue", "green"];
+colors.splice(1, 1);
+console.log("Answer no 14)",colors);
+// a) ["red", "green"] b) ["blue", "green"] c) ["red", "blue"] d) ["green"]
+//  correct a)  ["red", "green"]
+
+// Answer 15
+
+// How can you join an array into a single string separated by commas?
+let arr = ["a", "b", "c"];
+// a) arr.split(",") b) arr.toString() c) arr.join(",") d) Both b and c
+
+// correct c) arr.join(",")
+
+// Answer 16
+
+// What will this return?
+let str = "apple,banana,grape";
+let result = str.split(",");
+console.log("Answer no 16)",result[1]);
+// a) apple b) banana c) grape d) undefined
+
+// correct b) banana
+
+// Answer 17
+
+// Which method checks if an array contains a certain value?
+// a) search() b) includes() c) find() d) index()
+
+// correct b) includes
+
+// Answer 18
+
+// What does indexOf() return if the element is not found?
+// a) 0 b) null c) undefined d) -1
+
+// correct c -1
+
+// Answer 19
+
+// What does this code do?
+let numbers = [1, 2, 3, 4];
+numbers.splice(2, 0, 99);
+console.log("Answer no 19)",numbers);
+// a) Adds 99 at index 2 b) Removes 2 elements c) Replaces 2 elements with 99 d) Removes all
+// elements after index 2
+
+// correct a) Adds 99 at index 2
+
+// Answer 20
+
+// What will be the output of this code?
+let greeting = "Hello";
+console.log("Answer no 20)",greeting.charAt(1));
+// a) H b) e c) l d) o
+
+// correct a) e
